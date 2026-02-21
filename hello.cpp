27d4a1d8d31e613183d42bc717083c46f557ec3d
arthur04csc <arthurbezerraalbuquerque@gmail.com>
2026-02-21 12:46:09 +0000

    Aula 1

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(void){
+    int a, b, soma;
+
+    cout << "Qual o primeiro número?";
+    cin >> a;
+
+    cout << "Qual o segundo número?";
+    cin >> b;
+
+    soma = a + b;
+
+    cout << "A soma é " << soma << endl; 
+    return 0;
+}
