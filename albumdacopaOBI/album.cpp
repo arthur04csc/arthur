53e510deca8b1637089efdbdaafd068839f5516f
arthur04csc <arthurbezerraalbuquerque@gmail.com>
2026-03-21 12:37:55 +0000

    Aula 5

---
+++ albumdacopaOBI/album.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // não terminei ainda
+
+using namespace std;
+
+int main(void){
+    int n, m, x;
+
+    1 <= n <= 100;
+    1 <= m <= 300;
+    1 <= x <= n;
+
+    cout << "Quantas figurinhas tem no álbum?" << "\n";
+    cin >> n;
+
+    cout << "Quais as figuri"
+
+    cout << "Então falta " << 10 - m << " figurinhas para você." << "\n";
+    
+    cout << "Quais as figurinhas que você tem? "
+    cin >> 
+    
+}
